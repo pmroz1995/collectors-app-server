46086c41a44645f23446a4a2965cf83cb2a590db
mrozik52 <pmroz1995@gmail.com>
2021-11-20 15:34:46 +0100

    Adding login feature.

---
+++ dietApp-server/src/main/java/com/example/collectorsApp/login/models/Role.java
@@ -0,0 +1,50 @@
+package com.example.collectorsApp.login.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
+    private ERole name;
+
+    public Role() {
+
+    }
+
+    public Role(ERole name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public ERole getName() {
+        return name;
+    }
+
+    public void setName(ERole name) {
+        this.name = name;
+    }
+
+    public Role id(Integer id) {
+        this.id = id;
+        return this;
+    }
+
+    public Role name(ERole name) {
+        this.name = name;
+        return this;
+    }
+}
+
---
+++ dietApp-server/src/main/java/com/example/collectorsApp/login/repository/RoleRepository.java
@@ -0,0 +1,13 @@
+package com.example.collectorsApp.login.repository;
+
+import com.example.collectorsApp.login.models.ERole;
+import com.example.collectorsApp.login.models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(ERole name);
+}
